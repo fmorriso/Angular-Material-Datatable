9270fd5ee0c2bda5cf26b501165fd740e42b8995
Fred Morrison <fpmorrison@verizon.net>
2019-06-17 11:53:55 -0400

    modified table dropdown to use ng-template

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,21 +1,23 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { CombinedComponent } from './combined/combined.component';
 import { WithfilteringComponent } from './withfiltering/withfiltering.component';
 import { WithpaginationComponent } from './withpagination/withpagination.component';
 import { WithsortingComponent } from './withsorting/withsorting.component';
 import { DefaultComponent } from './default/default.component';
 
-const routeOptions: ExtraOptions = {enableTracing: true};
+const routeOptions: ExtraOptions = { enableTracing: true };
 const appRoutes: Routes = [
-    { path: '', redirectTo: 'Default', pathMatch: 'full' },
-    { path: 'Default', component: DefaultComponent },
-    { path: 'WithPagination', component: WithpaginationComponent },
-    { path: 'WithFiltering', component: WithfilteringComponent },
-    { path: 'WithSorting', component: WithsortingComponent },
-    { path: 'Combined', component: CombinedComponent }
-
+  { path: '', redirectTo: 'Default', pathMatch: 'full' },
+  { path: 'Default', component: DefaultComponent, data: { label: 'Default' } },
+  { path: 'WithPagination', component: WithpaginationComponent, data: { label: 'With Pagination' }  },
+  { path: 'WithFiltering', component: WithfilteringComponent, data: { label: 'With Filtering' }  },
+  { path: 'WithSorting', component: WithsortingComponent, data: { label: 'With Sorting' }  },
+  { path: 'Combined', component: CombinedComponent, data: { label: 'Combined' }  }
 ];
 
-export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes, routeOptions);
+export const routing: ModuleWithProviders = RouterModule.forRoot(
+  appRoutes,
+  routeOptions
+);
