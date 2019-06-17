f6a3e8a9fce355160c1e7bc4c6839ec7cff6ddcc
Fred Morrison <fpmorrison@verizon.net>
2019-06-17 12:06:06 -0400

    revised comment to reflect actual usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,11 +18,11 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // build the navigation links using the Angular route config that was defined in app-routing.module.ts
+    // build the navigation links using the Angular route config that was built from routes.ts
     // and dependency-injected into this component, which effectively shares that route information without
     // having to define a separate Angular service component.
     for (const route of this.router.config) {
-      // we only care about the ones that have a data.label since the others are not going to be rendered in our navigation bar
+      // we only care about the ones that have a data.label since the others are not going to be rendered in our navigation area
       if (route.data && route.data.label) {
         const link = {
           path: `/${route.path}`,
